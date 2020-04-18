ada5405e1316605eb087fe100557a25247715a80
Antoine-Alexis Bourdon <antoine-alexis_bourdon@ens.univ-artois.fr>
2020-04-18 23:00:38 +0200

    Update main(file is Ok), Update Readme, add files examples,

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+###########################################
+#
+# This is a sudoku solver in python.
+#
+# Author : Antoine-Alexis Bourdon
+# Link : https://github.com/antoinealexisb/sudoku_solver
+# Version : 0.1.3
+# Year : 2020-04
+# Dependency : sys
+#
+###########################################
+
 import sys
 
-board_test = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
+'''The function use recursion to find the right value in board
+Arguments:
+    --board : Array(board of sudoku)
+Return:
+    - : boolean (True = board correctly, False = not correctly)
+'''
 def solve(board):
     find = findEmpty(board)
     if not find:
@@ -45,7 +51,7 @@ Function that checks whether the digit at a position is correct.
 Arguments:
     --board : --Array() board of sudoku.
     --num : int() digit.
-    --pos : coordinates. 
+    --pos : coordinates.
 '''
 def valid(board, num, pos):
     for i in range(9):
@@ -70,6 +76,7 @@ Return:
     None.
 '''
 def printBoard(board):
+    print("\n")
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
@@ -88,27 +95,52 @@ Return:
     - : --Array (new board of sudoku).
 '''
 def substitution(boardMain):
-    tmp= [[0] * 9]*9
+    if (len(boardMain) != 81):
+        print("Error : You have not copied your grid board correctly.")
+    tmp=[]
     for i in range(9):
+        tmp.append([])
         for j in range(9):
-            tmp[i][j] = int(boardMain[i*9+j])
+            tmp[i].append(int(boardMain[i*9+j]))
     return tmp
 
+'''Function that loads the grid board file.
+Arguments :
+    -file : the file specified in the command
+Return :
+    - board : (the generated board)
+'''
+def charger(file):
+    print("\n ---- Filename : ",file)
+    try:
+        text_file = open(file, "r")
+    except:
+        print("Error, File Not found ! : ", file)
+        exit(1)
+    tmp = text_file.readlines()
+    if (len(tmp) != 9):
+        print("Error : Your board does not contain 9 lines")
+        exit(1)
+    if (len(tmp[0]) != 18):
+        print("Error : Your board does not contain 9 colomns")
+        exit(1)
+    return "".join(tmp).replace(" ", "").replace("\n","")
 
-def main(board):
+
+
+def main():
     '''Main function.
     '''
     if (len(sys.argv) == 1 ):
         boardMain = str(input("Give your sudoku board :"))
-        board = substitution(boardMain)
     elif (len(sys.argv) == 2):
-        pass #with the file (in progress)
+        boardMain = charger(sys.argv[1])
     else:
         return 1
+    board = substitution(boardMain)
     printBoard(board)
     solve(board)
-    print("\n\n")
     printBoard(board)
 
 if __name__ == '__main__':
-  main(board_test)
+  main()
